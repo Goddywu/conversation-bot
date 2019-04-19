9f2b02482745cd5a0377ddd83813c17e0d0a4be8
Goddy <wucs@ruyi.ai>
2019-04-19 18:44:55 +0800

    init

---
+++ js/about.14006158.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var o=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("editor")],1)},a=[],s=n("ceb0"),u={name:"About",components:{editor:s["a"]}},c=u,i=n("2877"),l=Object(i["a"])(c,o,a,!1,null,null,null);e["default"]=l.exports}}]);
